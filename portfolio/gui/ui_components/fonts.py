41db3a16a9259ca1a9410c8ff14d3afd87ce329d
timeerr <timeerr@tuta.io>
2021-07-13 11:29:36 +0200

    added fonts

--- portfolio/gui/ui_components/fonts.py
+++ portfolio/gui/ui_components/fonts.py
@@ -4,71 +4,62 @@ from PyQt5.QtGui import QFont
 
 
 class TitleFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
-        self.setPointSize(45)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setPointSize(80)
         self.setBold(True)
 
 
+class DescriptionFont(QFont):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setPointSize(14)
+        self.setWeight(QFont.Light)
+
+
 class SubtitleFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setPointSize(18)
         self.setBold(True)
         self.setItalic(True)
 
 
 class BoldFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setBold(True)
 
 
 class DateButtonFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setPointSize(8)
 
 
 class UpdateButtonFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setBold(True)
 
 
 class AccountBalanceHeaderFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setFamily("Roboto")
         self.setPointSize(11)
         self.setItalic(True)
 
 
 class AccountBalanceTextFont(QFont):
-
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setFamily("Noto Sans")
         self.setPointSize(40)
 
 
 class TokenBalanceFont(QFont):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.setFamily("Roboto")
         self.setPointSize(20)
