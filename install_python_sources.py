d917501cb8336a929eb2801b57990ba03d88732c
timeerr <timeerr@tuta.io>
2021-10-22 13:28:08 +0200

    Improved installation from python sources

--- install_python_sources.py
+++ install_python_sources.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 import sys
-import sys
 import os
 import shutil
-import logging
 
 
 def install_dependencies():
@@ -12,30 +10,29 @@ def install_dependencies():
 
 
 def initialize():
+    from portfolio import logger
     from portfolio.utils import confighandler
+    from portfolio.utils.confighandler import Paths
     from portfolio.utils.appdirs import user_data_dir
 
     confighandler.initial_setup()
 
-    # Translation
-    CONFIG_PATH = confighandler.get_config_path()
-    for file in os.listdir():
-        if ".qm" in file:
-            source = os.path.join(file)
-            dest = os.path.join(CONFIG_PATH, file)
-            shutil.copy(source, dest)
-            logging.info("Moved %s translation to %s", source, dest)
-
-    RESOURCES_PATH = confighandler.get_user_data_path()
+    # ---- Move resources to filesystem----
+    RESOURCES_PATH = Paths.RESOURCES_PATH
+    # Remove previous resources
     if 'portfolio' in os.listdir(user_data_dir()):
-        logging.info("Deleting previous resources: %s", RESOURCES_PATH)
+        logger.info(f"Deleting previous resources: {RESOURCES_PATH}")
         shutil.rmtree(RESOURCES_PATH)
-    # Copy resources to RESOURCES_PATH
+    # Translation files
+    for file in os.listdir('tanslations'):
+        if ".qm" in file:
+            dest = os.path.join(RESOURCES_PATH, file)
+            shutil.copy(file, dest)
+            logger.info(f"- Moved {file} translation to {dest}")
+    # Assets
     shutil.copytree(os.path.join('resources'), RESOURCES_PATH)
-    for file in os.listdir(RESOURCES_PATH):
-        if ".py" in file:
-            os.remove(os.path.join(RESOURCES_PATH, file))
-    logging.info("Created resources folder on %s", RESOURCES_PATH)
+
+    logger.info("Created resources folder on {RESOURCES_PATH}")
 
 
 if __name__ == "__main__":
