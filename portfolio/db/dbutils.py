67e954047239861d251902a6550424fc90b9cbd2
timeerr <timeerr@tuta.io>
2022-02-15 16:10:44 +0100

    refactor

--- portfolio/db/dbutils.py
+++ portfolio/db/dbutils.py
@@ -7,21 +7,19 @@ class DBHandler:
     F_PATH_TO_DB = os.path.join('database', 'portfolio.db')
 
 
-def create_connection_f(path_to_db=DBHandler.F_PATH_TO_DB):
-    conn = None
+def create_connection(path_to_db):
     try:
         conn = sqlite3.connect(path_to_db)
     except sqlite3.OperationalError:
         import traceback
         print(traceback.format_exc(), path_to_db)
+        return
     return conn
 
 
-def create_connection_c(path_to_db=DBHandler.C_PATH_TO_DB):
-    conn = None
-    try:
-        conn = sqlite3.connect(path_to_db)
-    except sqlite3.OperationalError:
-        import traceback
-        print(traceback.format_exc(), path_to_db)
-    return conn
+def create_connection_f():
+    return create_connection(path_to_db=DBHandler.F_PATH_TO_DB)
+
+
+def create_connection_c():
+    return create_connection(path_to_db=DBHandler.C_PATH_TO_DB)
